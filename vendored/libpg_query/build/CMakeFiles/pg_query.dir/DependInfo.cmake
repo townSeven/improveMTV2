395d92de053da090e98fed857511f8aa1f9a856a
townSeven <gzt1277@163.com>
2024-03-03 16:56:57 +0800

    first commit

---
+++ vendored/libpg_query/build/CMakeFiles/pg_query.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query.c" "CMakeFiles/pg_query.dir/src/pg_query.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query_fingerprint.c" "CMakeFiles/pg_query.dir/src/pg_query_fingerprint.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query_fingerprint.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query_json.c" "CMakeFiles/pg_query.dir/src/pg_query_json.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query_json.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query_json_plpgsql.c" "CMakeFiles/pg_query.dir/src/pg_query_json_plpgsql.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query_json_plpgsql.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query_normalize.c" "CMakeFiles/pg_query.dir/src/pg_query_normalize.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query_normalize.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query_parse.c" "CMakeFiles/pg_query.dir/src/pg_query_parse.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query_parse.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/pg_query_parse_plpgsql.c" "CMakeFiles/pg_query.dir/src/pg_query_parse_plpgsql.c.o" "gcc" "CMakeFiles/pg_query.dir/src/pg_query_parse_plpgsql.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/contrib_pgcrypto_sha1.c" "CMakeFiles/pg_query.dir/src/postgres/contrib_pgcrypto_sha1.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/contrib_pgcrypto_sha1.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_catalog_namespace.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_catalog_namespace.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_catalog_namespace.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_catalog_pg_proc.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_catalog_pg_proc.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_catalog_pg_proc.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_commands_define.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_commands_define.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_commands_define.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_lib_stringinfo.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_lib_stringinfo.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_lib_stringinfo.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_libpq_pqcomm.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_libpq_pqcomm.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_libpq_pqcomm.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_bitmapset.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_bitmapset.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_bitmapset.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_copyfuncs.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_copyfuncs.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_copyfuncs.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_equalfuncs.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_equalfuncs.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_equalfuncs.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_list.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_list.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_list.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_makefuncs.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_makefuncs.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_makefuncs.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_nodeFuncs.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_nodeFuncs.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_nodeFuncs.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_nodes_value.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_value.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_nodes_value.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_parser_gram.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_gram.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_gram.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_parser_keywords.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_keywords.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_keywords.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_parser_kwlookup.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_kwlookup.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_kwlookup.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_parser_parse_expr.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_parse_expr.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_parse_expr.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_parser_parser.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_parser.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_parser.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_parser_scansup.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_scansup.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_parser_scansup.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_postmaster_postmaster.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_postmaster_postmaster.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_postmaster_postmaster.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_storage_ipc_ipc.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_storage_ipc_ipc.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_storage_ipc_ipc.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_tcop_postgres.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_tcop_postgres.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_tcop_postgres.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_adt_datum.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_datum.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_datum.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_adt_expandeddatum.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_expandeddatum.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_expandeddatum.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_adt_format_type.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_format_type.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_format_type.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_adt_ruleutils.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_ruleutils.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_adt_ruleutils.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_error_assert.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_error_assert.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_error_assert.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_error_elog.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_error_elog.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_error_elog.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_init_globals.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_init_globals.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_init_globals.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_mb_encnames.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mb_encnames.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mb_encnames.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_mb_mbutils.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mb_mbutils.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mb_mbutils.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_mb_wchar.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mb_wchar.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mb_wchar.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_misc_guc.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_misc_guc.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_misc_guc.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_mmgr_aset.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mmgr_aset.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mmgr_aset.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_utils_mmgr_mcxt.c" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mmgr_mcxt.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_backend_utils_mmgr_mcxt.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_common_psprintf.c" "CMakeFiles/pg_query.dir/src/postgres/src_common_psprintf.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_common_psprintf.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_pl_plpgsql_src_pl_comp.c" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_comp.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_comp.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_pl_plpgsql_src_pl_funcs.c" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_funcs.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_funcs.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_pl_plpgsql_src_pl_gram.c" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_gram.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_gram.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_pl_plpgsql_src_pl_handler.c" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_handler.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_handler.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_pl_plpgsql_src_pl_scanner.c" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_scanner.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_pl_plpgsql_src_pl_scanner.c.o.d"
+  "/home/town/lingo-db/vendored/libpg_query/src/postgres/src_port_qsort.c" "CMakeFiles/pg_query.dir/src/postgres/src_port_qsort.c.o" "gcc" "CMakeFiles/pg_query.dir/src/postgres/src_port_qsort.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vendored/libpg_query/build/CMakeFiles/pg_query.dir/src/postgres/src_backend_commands_define.c.o.d
@@ -0,0 +1,152 @@
+CMakeFiles/pg_query.dir/src/postgres/src_backend_commands_define.c.o: \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/src_backend_commands_define.c \
+ /usr/include/stdc-predef.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/postgres.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/c.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/postgres_ext.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/pg_config_ext.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/pg_config.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/pg_config_manual.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/pg_config_os.h \
+ /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/12/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/lib/gcc/x86_64-linux-gnu/12/include/stdint.h \
+ /usr/include/stdint.h /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/locale.h /usr/include/x86_64-linux-gnu/bits/locale.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/port.h \
+ /usr/include/ctype.h /usr/include/netdb.h /usr/include/netinet/in.h \
+ /usr/include/x86_64-linux-gnu/sys/socket.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_iovec.h \
+ /usr/include/x86_64-linux-gnu/bits/socket.h \
+ /usr/include/x86_64-linux-gnu/bits/socket_type.h \
+ /usr/include/x86_64-linux-gnu/bits/sockaddr.h \
+ /usr/include/x86_64-linux-gnu/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types_64.h \
+ /usr/include/asm-generic/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h \
+ /usr/include/x86_64-linux-gnu/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_osockaddr.h \
+ /usr/include/x86_64-linux-gnu/bits/in.h /usr/include/rpc/netdb.h \
+ /usr/include/x86_64-linux-gnu/bits/netdb.h /usr/include/pwd.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/elog.h \
+ /usr/include/setjmp.h /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct___jmp_buf_tag.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/errcodes.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/palloc.h \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/catalog/namespace.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/primnodes.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/access/attnum.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/pg_list.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/nodes.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/lock.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/backendid.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/lwlock.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/lib/ilist.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/s_lock.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/port/atomics.h \
+ /usr/lib/gcc/x86_64-linux-gnu/12/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/12/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/port/atomics/arch-x86.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/port/atomics/generic-gcc.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/port/atomics/fallback.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/port/atomics/generic.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/shmem.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/hsearch.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/commands/defrem.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/catalog/objectaddress.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/acl.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/access/htup.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/itemptr.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/block.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/off.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/itemid.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/parsenodes.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/bitmapset.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/lockoptions.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/value.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/array.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/fmgr.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/expandeddatum.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/snapshot.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/lib/pairingheap.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/lib/stringinfo.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/storage/buf.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/relcache.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/access/tupdesc.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/catalog/pg_attribute.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/catalog/genbki.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/nodes/makefuncs.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/parser/parse_type.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/parser/parse_node.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/parser/scansup.h \
+ /home/town/lingo-db/vendored/libpg_query/src/postgres/include/utils/int8.h
